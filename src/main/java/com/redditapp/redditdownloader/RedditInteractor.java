44c7af1090dbff30c840be7cb559c9bc0dfd141d
Kapil-Baser <kapil.kumar.baser@gmail.com>
2025-02-09 07:50:43 +0530

    ...

--- src/main/java/com/redditapp/redditdownloader/RedditInteractor.java
+++ src/main/java/com/redditapp/redditdownloader/RedditInteractor.java
@@ -16,21 +16,15 @@ import java.util.Optional;
 
 public class RedditInteractor implements DownloadCompleteListener{
     private final RedditModel model;
-    //private RedditAPI redditAPI;
     private final RedditVideoService service;
 
     public RedditInteractor(RedditModel model) {
         this.model = model;
-        //this.redditAPI = new RedditAPI(ConfigLoader.getProperties());
         this.service = new RedditVideoService(ConfigLoader.getProperties());
     }
 
     public void start() {
         RedditVideoInfo videoInfo = this.service.getRedditVideoInfo(this.model.getRedditURL());
-//        RedditVideoInfo videoInfo = redditAPI.fetchVideoInfo(this.model.getRedditURL());
-//        if (videoInfo == null) {
-//            throw new RuntimeException("Given URL does not contain a video");
-//        }
         Downloader downloader = new Downloader(this);
         downloader.download(videoInfo, this.model.getDirectoryPath() + "\\");
     }
